0eec03be534731b73ae0fe834e09a026a276d117
AleksanteriA <60737700+AleksanteriA@users.noreply.github.com>
2023-06-06 03:22:09 +0300

    Update pattern.c

--- pikkuprojektit/c/pattern.c
+++ pikkuprojektit/c/pattern.c
@@ -3,11 +3,11 @@
 int main()
 {
     int i, j, n = 6;
-    for(i = 1; 1<=n; i++)
+    for(i=1; 1<=n; i++)
     {
-        for (j = n; j >= 1; j--)
+        for (j=n; j>=1; j--)
         {
-            if(i >= j)
+            if(i>=j)
                 printf("* ");
             else
                 printf(" ");
