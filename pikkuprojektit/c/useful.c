a91f871704e2509206b4b0bb130d75f8d9dcc81c
AleksanteriA <60737700+AleksanteriA@users.noreply.github.com>
2023-06-06 03:05:48 +0300

    renamed

---
+++ pikkuprojektit/c/useful.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Enter two numbers ");
+    scanf("%d%d", &a, &b);
+    c=a*b;
+    prinf("%d*%d is = %d", a, b, c);
+    return 0;
+}
